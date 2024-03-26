521dfc58cc3125f2032ab50c3fcf3b2efe30d8ad
Gabi <mgluisa9@gmail.com>
2024-03-26 16:33:32 +0200

    curs8 intermediar

---
+++ src/curs8/ArrayExample2.java
@@ -0,0 +1,25 @@
+package curs8;
+
+public class ArrayExample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Legume cartof = new Legume("Cartof dulce", 98);
+		cartof.printDetails();
+		
+		Legume[] arrayDeLegume = new Legume[3];
+		arrayDeLegume[0]=cartof;
+		//Legume[] arrayDeLegume = {cartof, null, null}
+		arrayDeLegume[0].printDetails();
+		arrayDeLegume[1] = new Legume("Praz", 55);
+		//Legume[] arrayDeLegume = {cartof, arrayDeLegume[1], null}
+		arrayDeLegume[1].printDetails();
+		arrayDeLegume[2] = new Legume("Pastarnac", 35);
+		
+		for(Legume leguma : arrayDeLegume) {
+			leguma.printDetails();
+		}
+	}
+
+}
