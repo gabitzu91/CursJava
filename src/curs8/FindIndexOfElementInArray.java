86e3482f3d9db8df0e9460a05c4b3ccc4e19e5f8
Gabi <mgluisa9@gmail.com>
2024-03-26 18:03:53 +0200

    curs8

---
+++ src/curs8/FindIndexOfElementInArray.java
@@ -0,0 +1,57 @@
+package curs8;
+/*luam un array 
+ * facem o metoda care sa afle indexul unui element pe care in dam ca param al metodei
+ * printam elementul si indexul
+ * rezolvam cu: for, for each, while, do while
+ * */
+public class FindIndexOfElementInArray {
+
+	static String[] culori = {"rosu", "alb", "galben", "verde", "negru", "maro", "albastru", "mov"};
+	//index 					0		1		2			3		4		5		6			7
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		rezolvareCuFor("mov");
+		rezolvareCuForEach("verde");
+		rezolvareCuWhile("rosu");
+		rezolvareCuDoWhile("galben");
+	}
+
+	public static void rezolvareCuFor(String culoare) {
+		for(int i=0; i<culori.length; i++) {
+			if(culori[i].equals(culoare)) {
+				System.out.println(culoare + " se afla la pozitia " + i);
+			}
+		}
+	}
+	
+	public static void rezolvareCuForEach(String culoare) {
+		int i=0;
+		for(String element : culori) {
+			if(element.equals(culoare)) {
+				System.out.println(culoare + " se afla la pozitia " + i);
+			}
+			i++;
+		}
+	}
+	
+	public static void rezolvareCuWhile(String culoare) {
+		int i=0;
+		while(i<culori.length) {
+			if(culori[i].equals(culoare)) {
+				System.out.println(culoare + " se afla la pozitia " + i);
+			}
+			i++;
+		}
+	}
+	
+	public static void rezolvareCuDoWhile(String culoare) {
+		int i=0;
+		do {
+			if(culori[i].equals(culoare)) {
+				System.out.println(culoare + " se afla la pozitia " + i);
+			}
+			i++;
+		}while(i<culori.length);
+	}
+}
---
+++ src/curs8/TestArrayEx3.java
@@ -0,0 +1,14 @@
+package curs8;
+
+public class TestArrayEx3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayExample3 obj = new ArrayExample3();
+		//System.out.println(obj.generateNumber());
+		obj.printArray(obj.generateArray());
+		
+	}
+
+}
