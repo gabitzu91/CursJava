d99441b44a3b4e27f28fe388dc01ce4f310fcd81
Gabi <mgluisa9@gmail.com>
2024-03-26 15:35:35 +0200

    tema curs 5 inter

--- src/curs5/TemperatureConverter.java
+++ src/curs5/TemperatureConverter.java
@@ -1,5 +1,25 @@
 package curs5;
 
+import java.util.Scanner;
+
 public class TemperatureConverter {
 
+	int nr;
+	double temp;
+	public int askTheUserForNumberOfConversions() {
+		System.out.println("Cate temperaturi vrei sa transformi?");
+		Scanner scan = new Scanner(System.in);
+		nr = scan.nextInt();
+		return nr;
+	}
+	public void convertToFarenheit() {
+		nr = askTheUserForNumberOfConversions();
+		for(int i=0;i<nr;i++) {
+			System.out.println("Introdu temperatura in grade Celsius:");
+			Scanner scan = new Scanner(System.in);
+			temp = scan.nextDouble();
+			System.out.println("Temperatura Celsius de " + temp + "grade este in Farenheit " +(temp+76.8) +" grade");
+		}
+	}
 }
+
